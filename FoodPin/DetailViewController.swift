f9ce59c06d2b4c2bec7d546ecec844c3470613bc
Managam Silalahi <managam.silalahi@gmail.com>
2016-03-22 14:39:59 +0700

    Memperbarui Class DetailViewController

--- FoodPin/DetailViewController.swift
+++ FoodPin/DetailViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UITableViewDataSouce, UITableViewDelegate {
+    
+    var restaurant:Restaurant!
     
     @IBOutlet var restaurantImageView : UIImageView!
     var restaurantImage : String!
@@ -17,13 +19,42 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        self.restaurantImageView.image = UIImage(named: restaurantImage)
+        self.restaurantImageView.image = UIImage(named: restaurant.image)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! DetailTableViewCell
+        
+        switch indexPath.row {
+        case 0:
+            cell.fieldLabel.text = "Name"
+            cell.valueLabel.text = restaurant.name
+        case 1:
+            cell.fieldLabel.text = "Location"
+            cell.valueLabel.text = restaurant.location
+        case 2:
+            cell.fieldLabel.text = "Type"
+            cell.valueLabel.text = restaurant.type
+        case 3:
+            cell.fieldLabel.text = "Been here"
+            cell.valueLabel.text = (restaurant.isVisited) ? "Yes, I've been here before" : "No, I've not"
+        default:
+            cell.fieldLabel.text = ""
+            cell.valueLabel.text = ""
+            
+        }
+    }
+    
     
 
     /*
