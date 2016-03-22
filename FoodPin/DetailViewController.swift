7361a5bff38f6808e57f3323e06efba317e57871
Managam Silalahi <managam.silalahi@gmail.com>
2016-03-22 17:57:55 +0700

    Stuck di auto layout

--- FoodPin/DetailViewController.swift
+++ FoodPin/DetailViewController.swift
@@ -33,6 +33,10 @@ class DetailViewController: UIViewController, UITableViewDataSource, UITableView
         
         // Set navigation bar title
         title = self.restaurant.name
+        
+//        Self Sizing Cell
+        tableView.estimatedRowHeight = 36.0;
+        tableView.rowHeight = UITableViewAutomaticDimension;
     }
     
     override func viewWillAppear(animated: Bool) {
