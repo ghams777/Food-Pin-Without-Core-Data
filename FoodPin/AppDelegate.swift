7a9ce9617fd3a294617d8c2fe0c3f715fc43dcc6
Managam Silalahi <managam.silalahi@gmail.com>
2016-03-23 14:13:29 +0700

    Menambahkan Toolbar sukses.

--- FoodPin/AppDelegate.swift
+++ FoodPin/AppDelegate.swift
@@ -29,6 +29,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Change status bar style
         UIApplication.sharedApplication().statusBarStyle = .LightContent
         
+//        Change Bar Button Item Appearance
+        UIBarButtonItem.appearance().tintColor = UIColor(red: 235.0/255.0, green: 73.0/255.0, blue: 27.0/255.0, alpha: 1.0)
+        UIToolbar.appearance().barTintColor = UIColor(red: 237.0/255.0, green: 240.0/255.0, blue: 243.0/255.0, alpha: 0.5)
+        
         return true
     }
 
