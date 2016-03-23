23741254bce9c7a7cef4fd60403dc17aab51c284
Managam Silalahi <managam.silalahi@gmail.com>
2016-03-23 17:31:15 +0700

    Animasi button Share View Controller sukses.

--- FoodPin/ShareViewController.swift
+++ FoodPin/ShareViewController.swift
@@ -12,6 +12,12 @@ class ShareViewController: UIViewController {
     
     @IBOutlet weak var backgroundImageView: UIImageView!
     
+    @IBOutlet weak var facebookButton: UIButton!
+    @IBOutlet weak var twitterButton: UIButton!
+    @IBOutlet weak var messageButton: UIButton!
+    @IBOutlet weak var emailButton: UIButton!
+    
+    
     override func viewDidLoad() {
         
         //        Membuat efek blur pada background
@@ -20,6 +26,29 @@ class ShareViewController: UIViewController {
         blurEffectView.frame = view.bounds
         backgroundImageView.addSubview(blurEffectView)
         
+        facebookButton.transform = CGAffineTransformMakeTranslation(0, 500)
+        emailButton.transform = CGAffineTransformMakeTranslation(0, -500)
+        
+        messageButton.transform = CGAffineTransformMakeTranslation(0, 500)
+        twitterButton.transform = CGAffineTransformMakeTranslation(0, -500)
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        
+        UIView.animateWithDuration(0.6, delay: 0.2, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.2, options: [], animations: {
+            
+
+            self.facebookButton.transform = CGAffineTransformMakeTranslation(0, 0)
+            
+            self.emailButton.transform = CGAffineTransformMakeTranslation(0, 0)
+            
+            self.messageButton.transform = CGAffineTransformMakeTranslation(0, 0)
+            
+            self.twitterButton.transform = CGAffineTransformMakeTranslation(0, 0)
+            
+            }, completion: nil)
     }
     
 }
