16860b8d7bb0ce0320faf61e20bc5f65b5fd3d8f
Managam Silalahi <managam.silalahi@gmail.com>
2016-03-23 16:39:29 +0700

    Membuat Animasi Dialog menggunakan UIView Animation sukses. 

--- FoodPin/ReviewViewController.swift
+++ FoodPin/ReviewViewController.swift
@@ -11,14 +11,34 @@ import UIKit
 class ReviewViewController: UIViewController {
     
     @IBOutlet weak var backgroundImageView: UIImageView!
-    
+    @IBOutlet weak var dialogView: UIView!
     
     override func viewDidLoad() {
         
+//        Membuat efek blur pada background
         let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.Dark)
         let blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView.frame = view.bounds
         backgroundImageView.addSubview(blurEffectView)
+        
+//        Membuat animasi dialog view
+        let scale = CGAffineTransformMakeScale(0.0, 0.0)
+        let translate = CGAffineTransformMakeTranslation(0, 500)
+        dialogView.transform = CGAffineTransformConcat(scale, translate)
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+//        UIView.animateWithDuration(<#T##duration: NSTimeInterval##NSTimeInterval#>, delay: <#T##NSTimeInterval#>, usingSpringWithDamping: <#T##CGFloat#>, initialSpringVelocity: <#T##CGFloat#>, options: <#T##UIViewAnimationOptions#>, animations: <#T##() -> Void#>, completion: <#T##((Bool) -> Void)?##((Bool) -> Void)?##(Bool) -> Void#>)
+        
+        UIView.animateWithDuration(0.7, delay: 0.0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.5, options: [], animations: {
+            
+            let scale = CGAffineTransformMakeScale(1, 1)
+            let translate = CGAffineTransformMakeTranslation(0, 0)
+            self.dialogView.transform = CGAffineTransformConcat(scale, translate)
+            
+            }, completion: nil)
         
     }
 }
